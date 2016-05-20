52e046ef9f9316ced9aa53e033abfe0a64123679
LauraKirby <laurakirby26@gmail.com>
2016-05-19 19:12:58 -0700

    add 'express static' to map url paths

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var app = express();
 app.set('superSecret', process.env.SECRET); // secret variable
 app.set('view engine', 'ejs');
 
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(morgan('dev')); // log requests to the console
