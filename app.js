65880af4f75dc3b42b7793900543dfbc9c045df7
LauraKirby <laurakirby26@gmail.com>
2016-05-19 19:52:20 -0700

    remove comments, app is working on heroku

--- app.js
+++ app.js
@@ -11,24 +11,17 @@ var apiRoutes = require('./routers/api');
 
 var app = express();
 
-// app.use(express.static(__dirname + '/')); // won't need this until i have static files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-
-app.use('/', basicRoutes);
-app.use('/api', apiRoutes);
-
-app.set('superSecret', process.env.SECRET); // secret variable
-
-
+app.set('superSecret', process.env.SECRET);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use('/', basicRoutes);
+app.use('/api', apiRoutes);
+
 app.use(morgan('dev')); // log requests to the console
 
-
-
-// use the `listen` method, provided by Express.js, to start the server
 app.listen(process.env.PORT || 3000, function () {
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
